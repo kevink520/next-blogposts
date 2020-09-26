7be4ded58ec2900ec3bbc66cbb531f8859ca252a
Kevin Kim <kevinkim75@gmail.com>
2020-09-26 18:35:47 -0400

    Enabled RSS feed

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,99 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Parser from 'rss-parser';
+import Head from 'next/head';
 
-export default function Home() {
+const Home = ({ posts }) => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Latest posts</title>
+        <link rel='icon' href='/favicon.ico' />
+        <link
+          rel='stylesheet'
+          href='https://cdn.jsdelivr.net/npm/@tailwindcss/ui@latest/dist/tailwind-ui.min.css'
+        />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <div>
+        <header className='bg-white shadow'>
+          <div className='max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8'>
+            <div className='flex justify-between'>
+              <h1 className='text-3xl font-bold leading-tight text-gray-900'>
+                Latest posts
+              </h1>
+            </div>
+          </div>
+        </header>
+        <main>
+          <div className='max-w-7xl mx-auto py-6 sm:px-6 lg:px-8'>
+            <div className='px-4 py-4 sm:px-0'>
+              <div className='border-4 rounded-lg'>
+                <div className='flex flex-col'>
+                  <div className='-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8'>
+                    <div className='align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg'>
+                      <table className='min-w-full'>
+                        <thead>
+                          <tr>
+                            <th className='px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider'>
+                              Post
+                            </th>
+                            <th className='px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider'>
+	                      Date
+                            </th>
+                          </tr>
+                        </thead>
+                        <tbody className='bg-white'>
+	                  {posts.sort((a, b) => new Date(b.date) - new Date(a.date)).map((value, index) => (
+                            <tr key={index}>
+                              <td className='px-6 py-4 whitespace-no-wrap border-b border-gray-200'>
+                                <div className='flex items-center'>
+                                  <div className='ml-4'>
+                                    <div className='text-sm leading-5 font-medium text-gray-900 underline'>
+                                       <a href={value.link}>{value.title}</a>
+                                    </div>
+                                    <div className='text-sm leading-5 text-gray-500'>
+                                      {value.name}
+                                    </div>
+                                  </div>
+                                </div>
+                              </td>
+                              <td className='px-6 py-4 whitespace-no-wrap border-b border-gray-200'>
+                                <div className='text-sm leading-5 text-gray-900'>
+                                  {new Date(value.date).toDateString()}
+                                </div>
+                                <div className='text-sm leading-5 text-gray-500'></div>
+                              </td>
+			    </tr>
+			  ))}
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export const getStaticProps = async context => {
+  const parser = new Parser();
+  const data = await parser.parseURL('https://flaviocopes.com/index.xml');
+  const posts = data.items.slice(0, 10).map(item => ({
+    title: item.title,
+    link: item.link,
+    date: item.isoDate,
+    name: 'Flavio Copes',
+  }));
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
+
+export default Home;
+
